388230a77fec2bca9c214c8e8330bd6d26949f4a
Adriaan <adriaanmarain300@gmail.com>
2019-10-11 14:18:37 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import '../css/app.css';
+
+require('./vendor/symfony');
+
+import Ignition from './Ignition';
+
+/* window.ignite = data => {
+    return new Ignition(data);
+}; */
+
+export default Ignition;
